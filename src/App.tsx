6c4350b150c2df75d34d0f58078c47f538e67cd0
josue rojas <josuerojas.rojas@gmail.com>
2023-11-09 12:34:15 -0500

    use custom hook for theme

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,15 @@ import { router } from './Routes';
 import { Menu } from './component/Menu';
 import { MENU_PROPS } from './constants/menu';
 import styles from './styles.module.scss';
-import { GlobalContext, initialContext } from './GlobalContext';
-import { useContext } from "react";
+import { GlobalContext } from './GlobalContext';
 import classNames from "classnames";
+import { useTheme } from "./hooks/useTheme";
 
 function App() {
-  const { theme } = useContext(GlobalContext);
+  const { theme } = useTheme();
 
   return (
-    <GlobalContext.Provider value={initialContext}>
+    <GlobalContext.Provider value={{ theme }}>
       <main className={classNames(styles.app, styles[theme])}>
         <Menu {...MENU_PROPS} theme={theme} />
         <div className={classNames(styles.main, styles[theme])}>
