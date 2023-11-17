9c3925702fd18de3afc3e3b980c21cee5595c19e
josue rojas <josuerojas.rojas@gmail.com>
2023-11-16 23:21:16 -0500

    [FEAT] Enable body and meta colors to switch with theme

--- src/hooks/useTheme.ts
+++ src/hooks/useTheme.ts
@@ -1,31 +1,47 @@
 import { useEffect, useState } from "react";
 import { Theme } from "../constants/theme";
 
-const isSystemDarkTheme = window.matchMedia("(prefers-color-scheme: dark)");
+type Colors = { dark: string, light: string; }
 
-export function useTheme() {
+const isSystemDarkTheme = window.matchMedia('(prefers-color-scheme: dark)');
+const LIGHT_THEME_COLOR = 'rgb(248, 249, 250)';
+const DARK_THEME_COLOR = 'rgb(28, 28, 28)';
+const DEFAULT_SYSTEM_COLOR = isSystemDarkTheme ? DARK_THEME_COLOR : LIGHT_THEME_COLOR;
+
+// set default colors
+document.querySelector("meta[name='theme-color']")?.setAttribute('content', DEFAULT_SYSTEM_COLOR);
+document.body.style.backgroundColor = DEFAULT_SYSTEM_COLOR;
+
+const setWholeTheme = (theme: Theme, colors: Colors, setTheme: React.Dispatch<React.SetStateAction<Theme>>) => {
+  const selectedColor = theme === Theme.dark ? colors.dark : colors.light;
+  document.querySelector("meta[name='theme-color']")?.setAttribute('content', selectedColor);
+  document.body.style.backgroundColor = selectedColor;
+  setTheme(theme);
+}
+
+export function useTheme(colors: Colors = { dark: DARK_THEME_COLOR, light: LIGHT_THEME_COLOR }) {
   const [theme, setTheme] = useState<Theme>(isSystemDarkTheme.matches ? Theme.dark : Theme.light)
   const [isSetBySystem, setIsSetBySystem] = useState<boolean>(true);
-  
+
   useEffect(() => {
     const setNextSystemTheme = ({ matches }: { matches: boolean}) => {
       if (isSetBySystem) {
         const themeSet = matches ? Theme.dark : Theme.light;
-        setTheme(themeSet);
+        setWholeTheme(themeSet, colors, setTheme);
       }
     }
 
     isSystemDarkTheme.addEventListener('change', setNextSystemTheme);
 
     return () => isSystemDarkTheme.removeEventListener('change', setNextSystemTheme);
-  }, [isSetBySystem, setIsSetBySystem]);
+  }, [isSetBySystem, setIsSetBySystem, colors]);
 
 
   const toggleTheme = (theme: Theme | 'System') => {
     if (theme === 'System') {
       setIsSetBySystem(true);
     } else {
-      setTheme(theme);
+      setWholeTheme(theme, colors, setTheme);
       setIsSetBySystem(false);
     }
   }
