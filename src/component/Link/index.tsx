4d6bb28b4e87b104c3a21648a6fd1c7c4a7038f3
josue rojas <10749061+josue-rojas@users.noreply.github.com>
2023-11-24 20:57:39 -0500

    [BUGFIX] Add light theme colors for link (#10)

--- src/component/Link/index.tsx
+++ src/component/Link/index.tsx
@@ -1,18 +1,23 @@
 import classNames from 'classnames';
 import { Link as RouterLink } from 'react-router-dom';
+import { Theme } from '../../constants/theme';
 import styles from './styles.module.scss';
 
-export interface LinkProps {
-  children: React.ReactNode;
-  className?: string;
+export interface LinkBase {
   href: string;
+  children: React.ReactNode;
   isInternal?: boolean;
-  onClick?: React.MouseEventHandler<HTMLAnchorElement> | undefined;
 }
 
-export const Link = ({ children, className, href, isInternal, onClick }: LinkProps) => { 
+export type LinkProps = LinkBase & {
+  className?: string;
+  onClick?: React.MouseEventHandler<HTMLAnchorElement> | undefined;
+  theme: Theme;
+}
+
+export const Link = ({ children, className, href, isInternal, onClick, theme }: LinkProps) => { 
   isInternal = isInternal || false;
-  const classes = classNames(styles.link, className);
+  const classes = classNames(styles.link, className, styles[theme]);
 
   if (isInternal) {
     return <RouterLink className={classes} to={href} onClick={onClick}>{children}</RouterLink>;
